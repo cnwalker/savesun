5627fba226b840d1eee8367011c9f732ccfd1ff0
Christopher Walker <cnwalker@uchicago.edu>
2017-04-29 13:03:44 -0700

    added django to setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+setup(name='SaveSun',
+      version='0.0.1',
+      description='Solar power usage monitor for the HI-SEAS project',
+      author='Christopher Walker',
+      author_email='cnwalker@uchicago.edu',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Django>=1.10'],
      )
